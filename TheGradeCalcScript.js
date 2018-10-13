d2d8a37d94c1452eb5a507fa27c7967ad5c630b5
Nathan Ng <nathann178@yahoo.com>
2018-10-13 18:20:27 -0400

    removed id tags/finalized js part with letter

--- TheGradeCalcScript.js
+++ TheGradeCalcScript.js
@@ -4,28 +4,11 @@ function reload(){
 function gradeCalc(){
 
     var grades = [93, 90, 87, 83, 80, 77, 73, 70, 67, 63, 60];
+    var letterGrades = ["A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-"];
 
     //obtaining information
     var className = document.getElementById("className").value; 
     var finalex = document.getElementById("finalex").value * 1;
-    
-    //remove later
-    // var values = [];
-    // var g1 = document.getElementById("g1").value * 1;
-    // var g2 = document.getElementById("g2").value * 1;
-    // var p1 = document.getElementById("p1").value * 1;
-    // var p2 = document.getElementById("p2").value * 1;
-    // values.push(g1);
-    // values.push(g2);
-    // values.push(p1);
-    // values.push(p2);
-    // var validcounter = 0;
-    // for(i = 0; i < 4; i++) {
-    //      var temp = values.pop();
-    //      if (temp <= 0 | temp > 100) {
-    //          validcounter++;
-    //      }
-    // }
 
     var validcounter = 0;
 
@@ -69,7 +52,7 @@ function gradeCalc(){
 
     //calculates the final grade based on what has already been taken
     var finalGrade = (existingScores/weights)*100;
-    document.getElementById("currentGrade").innerHTML = "Your final current grade is: " + finalGrade;
+    document.getElementById("currentGrade").innerHTML = "Your final current grade is: " + Number.parseFloat(finalGrade).toFixed(2) +"%.";
     
     //if the user wants to find out what they need to get on their next assignment
     if(finalex > 0) {
@@ -77,33 +60,8 @@ function gradeCalc(){
         for(i = 0; i < grades.length; i++) {
             var temp = (grades[i] * .01 * totalPercent) - existingScores;
             temp = (temp/(.01*finalex));
-            document.getElementById("get" + grades[i]).innerHTML = "You need to get a " + temp + " to get a " + grades[i];
+            document.getElementById("get" + grades[i]).innerHTML = "You need to get a " + Number.parseFloat(temp).toFixed(2) + "% to get a " + grades[i] + "% (" + letterGrades[i] + ").";
         }
-        
-        // var toget93 = ((93/100) * totalPercent) - existingScores;
-        // toget93 = (toget93/(.01*finalex));
-        // document.getElementById("get93").innerHTML = "You need to get a " + toget93 + " to get a 93";
-        // var toget90 = ((90/100) * totalPercent) - existingScores;
-        // toget90 = (toget90/(.01*finalex));
-        // document.getElementById("get90").innerHTML = "You need to get a " + toget90 + " to get a 90";
-        // var toget87 = ((87/100) * totalPercent) - existingScores;
-        // toget87 = (toget87/(.01*finalex));
-        // document.getElementById("get87").innerHTML = "You need to get a " + toget87 + " to get a 87";
-        // var toget83 = ((83/100) * totalPercent) - existingScores;
-        // toget83 = (toget83/(.01*finalex));
-        // document.getElementById("get83").innerHTML = "You need to get a " + toget83 + " to get a 83";
-        // var toget80 = ((80/100) * totalPercent) - existingScores;
-        // toget80 = (toget80/(.01*finalex));
-        // document.getElementById("get80").innerHTML = "You need to get a " + toget80 + " to get a 80";
     }
-
-    //test for grades in array
-
-    //document.getElementById("get83").innerHTML = grayeds[0].value*1;
-    //document.getElementById("get80").innerHTML = grayeds[1].value*1;
-    
-
-    //var test = document.getElementById("reloadButton");
-    //test.value = "Try another class!";
     document.getElementById("texttoshow").style.display = "inline";
 }
