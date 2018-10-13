d217f81175cd0cc1d8a3f6b63a053d22461f3e0f
Nathan Ng <nathann178@yahoo.com>
2018-10-13 16:19:29 -0400

    simplified variables to help with ease of readbility

--- TheGradeCalcScript.js
+++ TheGradeCalcScript.js
@@ -56,19 +56,21 @@ function gradeCalc(){
     finalGrade = (finalGrade/weights)*100;
     document.getElementById("currentGrade").innerHTML = "Your final current grade is: " + finalGrade;
     if(finalex > 0 && finalex <=100) {
-        var toget93 = ((93/100) * (p1+p2+finalex)) - (g1*p1*.01) - (g2*p2*.01);
+        var existingScores = (g1*p1*.01) + (g2*p2*.01);
+        var totalPercent = (p1+p2+finalex);
+        var toget93 = ((93/100) * totalPercent) - existingScores;
         toget93 = (toget93/(.01*finalex));
         document.getElementById("get93").innerHTML = "You need to get a " + toget93 + " to get a 93";
-        var toget90 = ((90/100) * (p1+p2+finalex)) - (g1*p1*.01) - (g2*p2*.01);
+        var toget90 = ((90/100) * totalPercent) - existingScores;
         toget90 = (toget90/(.01*finalex));
         document.getElementById("get90").innerHTML = "You need to get a " + toget90 + " to get a 90";
-        var toget87 = ((87/100) * (p1+p2+finalex)) - (g1*p1*.01) - (g2*p2*.01);
+        var toget87 = ((87/100) * totalPercent) - existingScores;
         toget87 = (toget87/(.01*finalex));
         document.getElementById("get87").innerHTML = "You need to get a " + toget87 + " to get a 87";
-        var toget83 = ((83/100) * (p1+p2+finalex)) - (g1*p1*.01) - (g2*p2*.01);
+        var toget83 = ((83/100) * totalPercent) - existingScores;
         toget83 = (toget83/(.01*finalex));
         document.getElementById("get83").innerHTML = "You need to get a " + toget83 + " to get a 83";
-        var toget80 = ((80/100) * (p1+p2+finalex)) - (g1*p1*.01) - (g2*p2*.01);
+        var toget80 = ((80/100) * totalPercent) - (existingScores;
         toget80 = (toget80/(.01*finalex));
         document.getElementById("get80").innerHTML = "You need to get a " + toget80 + " to get a 80";
     }
